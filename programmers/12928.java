686d904f05adc7f2635134642d77cb3e00b63075
gps03155 <gps03155@naver.com>
2020-08-23 12:22:24 +0900

    Update 12928.java

--- programmers/12928.java
+++ programmers/12928.java
@@ -6,7 +6,7 @@ class Solution {
         int answer = 0;
         
         OptionalInt sum = IntStream.range(1, n+1).filter(m -> n % m == 0).reduce((total, num) -> total + num);
-		answer = sum.isPresent()? sum.getAsInt() : 0;
+	answer = sum.isPresent()? sum.getAsInt() : 0;
         
         return answer;
     }
