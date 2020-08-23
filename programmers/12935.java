adb21d6340ae0ce7c7a89e25bf8b60fe2444911f
gps03155 <gps03155@naver.com>
2020-08-23 12:57:36 +0900

    Update 12935.java

--- programmers/12935.java
+++ programmers/12935.java
@@ -2,22 +2,23 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr) {
-        // case1
-        /*
+        // case1 - 속도가 더 빠름
         int[] answer = Arrays.stream(arr).filter(m -> m != Arrays.stream(arr).min().getAsInt()).toArray();
 		
-        if(answer.length == 0) {
-          answer = new int[arr.length];
-          answer[answer.length - 1] = -1;
-        }
-        */
+	if(answer.length == 0) {
+		answer = new int[arr.length];
+		answer[answer.length - 1] = -1;
+	}
+        
         
         // case2
+        /*
         int[] answer = {};
         
         if(arr.length <= 1) return answer = new int[]{-1};
         answer = Arrays.stream(arr).filter(m -> m != Arrays.stream(arr).min().getAsInt()).toArray();
-        
+        */
         return answer;
+        
     }
 }
