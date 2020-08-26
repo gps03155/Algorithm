042cb6d01b08237b5888ae83f9fe9ea28ae7bc9a
gps03155 <gps03155@naver.com>
2020-08-26 23:49:43 +0900

    Create 42576.java

---
+++ programmers/42576.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        Map<Object, Long> count = Arrays.asList(participant).stream().collect(Collectors.groupingBy(m -> m, Collectors.counting()));
+		Map<Object, Long> competionCount =  Arrays.asList(completion).stream().collect(Collectors.groupingBy(m -> m, Collectors.counting()));
+        
+        for(Object key : count.keySet()){
+            if(competionCount.containsKey(key)){
+                if(!count.get(key).equals(competionCount.get(key))){
+                    answer = String.valueOf(key);
+                    break;
+                }
+            }
+            else{
+                answer = String.valueOf(key);
+            }
+        }
+        
+        return answer;
+    }
+}
