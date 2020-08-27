28008cf1e64cc36a7aa7d2b4dc954710566ba9af
gps03155 <gps03155@naver.com>
2020-08-27 23:03:03 +0900

    Create 12933.java

---
+++ programmers/12933.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        String num = "";
+        
+        String[] arr = String.valueOf(n).split("");
+		Arrays.sort(arr, Collections.reverseOrder());
+		
+		for(String str : arr) {
+			num += str;
+		}
+        
+        answer = Long.valueOf(num);
+        
+        return answer;
+    }
+}
