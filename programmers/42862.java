575cfaf077dce23118819fc95a8df74eb4ad6f45
gps03155 <gps03155@naver.com>
2020-09-05 17:15:13 +0900

    Update 42862.java

--- programmers/42862.java
+++ programmers/42862.java
@@ -6,29 +6,29 @@ class Solution {
         int answer = n;
         
         List<Integer> lostList = Arrays.stream(lost).sorted().boxed().collect(Collectors.toList());
-		List<Integer> reserveList = Arrays.stream(reserve).sorted().boxed().collect(Collectors.toList());
+	List<Integer> reserveList = Arrays.stream(reserve).sorted().boxed().collect(Collectors.toList());
 		
-		List<Integer> newLostList = lostList.stream().filter(m -> !reserveList.contains(m)).collect(Collectors.toList());
-		List<Integer> newReserveList = reserveList.stream().filter(m -> !lostList.contains(m)).collect(Collectors.toList());
+	List<Integer> newLostList = lostList.stream().filter(m -> !reserveList.contains(m)).collect(Collectors.toList());
+	List<Integer> newReserveList = reserveList.stream().filter(m -> !lostList.contains(m)).collect(Collectors.toList());
 		
-		for(int i=1;i<=n;i++) {
-			if(newLostList.contains(i)) {
-				if(newReserveList.contains(i)) {
-					newReserveList.remove(newReserveList.indexOf(i));
-					continue;
-				}
-				else if(newReserveList.contains(i-1)) {
-					newReserveList.remove(newReserveList.indexOf(i-1));
-					continue;
-				}
-				else if(newReserveList.contains(i+1)) {
-					newReserveList.remove(newReserveList.indexOf(i+1));
-					continue;
-				}
-				
-				answer--;
+	for(int i=1;i<=n;i++) {
+		if(newLostList.contains(i)) {
+			if(newReserveList.contains(i)) {
+				newReserveList.remove(newReserveList.indexOf(i));
+				continue;
 			}
+			else if(newReserveList.contains(i-1)) {
+				newReserveList.remove(newReserveList.indexOf(i-1));
+				continue;
+			}
+			else if(newReserveList.contains(i+1)) {
+				newReserveList.remove(newReserveList.indexOf(i+1));
+				continue;
+			}
+			
+			answer--;
 		}
+	}
         
         return answer;
     }
