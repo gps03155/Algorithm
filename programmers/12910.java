f684d935dc3391248eee3371b5b8d204948bbc4f
gps03155 <gps03155@naver.com>
2020-09-06 14:48:47 +0900

    Create 12910.java

---
+++ programmers/12910.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        
+        List<Integer> list = IntStream.of(arr).filter(m -> m % divisor == 0).sorted().boxed().collect(Collectors.toList());
+		if(list.isEmpty()) list.add(-1);
+        
+        answer = list.stream().mapToInt(m -> m).toArray();
+        
+        return answer;
+    }
+}
