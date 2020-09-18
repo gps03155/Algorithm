3b834421867c421c64095954b4d696b83f4922df
gps03155 <gps03155@naver.com>
2020-09-18 23:18:50 +0900

    Update 42577.java

--- programmers/42577.java
+++ programmers/42577.java
@@ -5,15 +5,14 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
-		Arrays.sort(phone_book);
+	Arrays.sort(phone_book);
 		
-		String phone = phone_book[0];
+	String phone = phone_book[0];
 		
-		List<String> result = Arrays.asList(phone_book).stream().filter(m -> String.valueOf(m).startsWith(String.valueOf(phone))).collect(Collectors.toList());
+	List<String> result = Arrays.asList(phone_book).stream().filter(m -> String.valueOf(m).startsWith(String.valueOf(phone))).collect(Collectors.toList());
 		
-		if(result.size() > 1) answer = false;
-		
-		
+	if(result.size() > 1) answer = false;
+			
         return answer;
     }
 }
