81793d31173190d4d77f329110215e4ce78df86c
gps03155 <gps03155@naver.com>
2020-10-29 20:34:31 +0900

    Create 68644.java

---
+++ programmers/68644.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        
+        Set<Integer> set = new HashSet<Integer>();
+		
+		for(int i=0;i<numbers.length;i++) {
+			List<Integer> otherNumberList = IntStream.of(numbers).boxed().collect(Collectors.toList());
+			otherNumberList.remove(i);
+
+			for(int otherNum : otherNumberList) {
+				set.add(numbers[i] + otherNum);
+			}
+		}
+		
+		answer = set.stream().sorted().mapToInt(Integer::intValue).toArray();
+		
+        return answer;
+    }
+}
