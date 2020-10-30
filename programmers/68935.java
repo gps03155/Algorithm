350b40e3df2510e468f7068e06c1c07887c1906b
gps03155 <gps03155@naver.com>
2020-10-30 16:57:26 +0900

    Create 68935.java

---
+++ programmers/68935.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        StringBuilder builder = new StringBuilder();
+		
+		while(true) {
+			builder.append(n%3);
+			n /= 3;
+			if(n == 0) break;
+		}
+		
+		// 3진법 
+//		builder.reverse();
+		
+    // 두번째 파라미터에 변환될 숫자의 진수 추가 
+		answer = Integer.parseInt(builder.toString(), 3);
+		
+        return answer;
+    }
+}
