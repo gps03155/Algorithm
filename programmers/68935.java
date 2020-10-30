24aaa8232a8e489fcfbd8827183a864c29b8d99c
gps03155 <gps03155@naver.com>
2020-10-30 16:57:48 +0900

    Update 68935.java

--- programmers/68935.java
+++ programmers/68935.java
@@ -4,17 +4,17 @@ class Solution {
         
         StringBuilder builder = new StringBuilder();
 		
-		while(true) {
-			builder.append(n%3);
-			n /= 3;
-			if(n == 0) break;
-		}
+	while(true) {
+		builder.append(n%3);
+		n /= 3;
+		if(n == 0) break;
+	}
 		
-		// 3진법 
-//		builder.reverse();
+	// 3진법 
+//	builder.reverse();
 		
     // 두번째 파라미터에 변환될 숫자의 진수 추가 
-		answer = Integer.parseInt(builder.toString(), 3);
+	answer = Integer.parseInt(builder.toString(), 3);
 		
         return answer;
     }
