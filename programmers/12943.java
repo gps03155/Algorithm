a062f6152462dfcab5953e9a73aecfc0406620b9
gps03155 <gps03155@naver.com>
2020-11-13 14:43:11 +0900

    Create 12943.java

---
+++ programmers/12943.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(double num) {
+        int count = 0;
+        
+        if(num == 1){
+            return 0;
+        }
+		
+		while(true) {
+           if(num == 1 || count >= 500) {
+				break;
+			}
+            
+			if(num%2 == 0) {
+				num /= 2;
+			}
+			else {
+				num *= 3;
+				num += 1;
+			}
+			
+			count++;
+		}
+		
+		int answer = count >= 500? -1 : count;
+        return answer;
+    }
+}
