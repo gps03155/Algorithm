917878b488350aefe6177855e6488e3ccdb54dc7
gps03155 <gps03155@naver.com>
2020-11-30 18:00:19 +0900

    Update 14681.java

--- java/14681.java
+++ java/14681.java
@@ -4,14 +4,14 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 		
-		int x = sc.nextInt();
-		int y = sc.nextInt();
+	int x = sc.nextInt();
+	int y = sc.nextInt();
 		
-		int quater = 1;
+	int quater = 1;
 		
-		if(x < 0 && y > 0) quater = 2;
-		if(x < 0 && y < 0) quater = 3;
-		if(x > 0 && y < 0) quater = 4;
+	if(x < 0 && y > 0) quater = 2;
+	if(x < 0 && y < 0) quater = 3;
+	if(x > 0 && y < 0) quater = 4;
         
         System.out.println(quater);
     }
