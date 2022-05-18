76273c92f682865676d65c56549c55244fdeeefe
gps03155 <gps03155@naver.com>
2022-05-18 22:49:22 +0900

    Create 2163.java

---
+++ baekjoon/2163.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		if(n == 1 && m == 1) {
+			System.out.println(0);
+		}
+		else {
+			System.out.println(n*m -1);
+		}
+	}
+}
