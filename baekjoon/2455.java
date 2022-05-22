4aa9ad7e0c96e0c9b7280221d1ffced3b610b40d
gps03155 <gps03155@naver.com>
2022-05-23 05:23:18 +0900

    Create 2455.java

---
+++ baekjoon/2455.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int people = 0;
+		int max = Integer.MIN_VALUE;
+		
+		for(int i=0; i<4; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int out = Integer.parseInt(st.nextToken());
+			int in = Integer.parseInt(st.nextToken());
+			
+			people -= out;
+			people += in;
+			
+			max = people > max? people : max;
+		}
+		
+		System.out.println(max);
+	}
+}
