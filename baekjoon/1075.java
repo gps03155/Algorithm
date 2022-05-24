77603e01b43347404dffa754e1d4c2d7a3ddff51
gps03155 <gps03155@naver.com>
2022-05-24 21:34:29 +0900

    Update 1075.java

--- baekjoon/1075.java
+++ baekjoon/1075.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		String n = sc.nextLine();
+		int n = sc.nextInt();
 		int f = sc.nextInt();
-		int num = Integer.parseInt(n.substring(0, n.length() - 2) + "0".repeat(2));
+		int num = n / 100 * 100;
 		
 		while(num % f != 0) {
 			num++;
