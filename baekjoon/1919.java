13f55f3738fad7a867cc5956f720448bda86cd48
gps03155 <gps03155@naver.com>
2022-08-02 19:11:01 +0900

    Update 1919.java

--- baekjoon/1919.java
+++ baekjoon/1919.java
@@ -9,7 +9,27 @@ import java.util.List;
 public class Main {
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    /**
+	    * 배열로 푸는 방식이 메모리, 시간 면에서 더 좋음
+	    **/
 		
+	    // 1. 배열 (14296KB  128ms)
+	    int[] alpha = new int[26];
+		
+	    char[] word1 = br.readLine().toCharArray();
+	    char[] word2 = br.readLine().toCharArray();
+		
+	    for(char cha : word1) {
+	        alpha[cha - 'a']++;
+	    }
+		
+	    for(char cha : word2) {
+	    	alpha[cha - 'a']--;
+	    }
+		
+	    System.out.println(Arrays.stream(alpha).map(m -> Math.abs(m)).sum());
+		
+	    // 2. for (14356KB	140ms)
 	    String[] word1= br.readLine().split("");
 	    String[] word2 = br.readLine().split("");
 		
