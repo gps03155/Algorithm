0148e5a3e4092f541b51e1ce49823708e37408bf
gps03155 <gps03155@naver.com>
2022-08-21 10:16:08 +0900

    Update 2153.java

--- baekjoon/2153.java
+++ baekjoon/2153.java
@@ -1,30 +1,28 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
+	public static int getNum(char alpha) {
+		if('a' <= alpha && alpha <= 'z') {
+			return alpha - 96;
+		}
 
-		Scanner sc = new Scanner(System.in);
+		return alpha - 38;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String word = br.readLine();
 		
-		String word = sc.nextLine();
 		int sum = 0;
 		
-		for(int i=0; i<word.length(); i++) {
-			char ch = word.charAt(i);
-			
-			if(Character.isUpperCase(ch)) {
-				sum += ch - 38;
-			}
-			else {
-				sum += ch - 96;
-			}
+		for(char cha : word.toCharArray()) {
+			sum += getNum(cha);
 		}
 		
 		Boolean isPrime = true;
-		
-		if(sum % 2 == 0) isPrime = false;
-		
+
 		for(int i=2; i<sum; i++) {
 			if(sum % i == 0) {
 				isPrime = false;
@@ -33,5 +31,5 @@ public class Main {
 		}
 		
 		System.out.println(isPrime? "It is a prime word." : "It is not a prime word.");
-    }
+	}
 }
