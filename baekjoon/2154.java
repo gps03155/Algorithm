86f02e152728f541c415e530a99878cf1fd29578
gps03155 <gps03155@naver.com>
2022-08-21 10:38:34 +0900

    Update 2154.java

--- baekjoon/2154.java
+++ baekjoon/2154.java
@@ -8,22 +8,12 @@ public class Main {
 
 		int num = Integer.parseInt(br.readLine());
 		StringBuilder builder = new StringBuilder();
-		
-		for(int i=1; i<=num; i++) {
+
+		for (int i = 1; i <= num; i++) {
 			builder.append(i);
 		}
-		
-		int addNum = num + 1;
-		
-		while(true) {
-			int index = builder.toString().indexOf(String.valueOf(num));
-			
-			if(index > -1) {
-				System.out.println(index + 1);
-				break;
-			}
 
-			builder.append(addNum);
-		}
+		int index = builder.toString().indexOf(String.valueOf(num));
+		System.out.println(index + 1);
 	}
 }
