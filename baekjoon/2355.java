fa1ea206aaa360aec65a7e1a66b9ee349750f994
gps03155 <gps03155@naver.com>
2022-08-22 21:43:47 +0900

    Update 2355.java

--- baekjoon/2355.java
+++ baekjoon/2355.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+		// 1. long (14228KB 128ms)
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		
+		long min = Math.min(a, b);
+		long max = Math.max(a, b);
+		
+		long sum = (max + min) * ((max - min) + 1) / 2;
+		
+		System.out.println(sum);
+		
+		// 2. BigInteger (17872KB 212ms)
 		Scanner sc = new Scanner(System.in);
 
 		BigInteger a = sc.nextBigInteger();
