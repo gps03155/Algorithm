f556d06429b7346247e38d131b1fcb6c4af9ae86
gps03155 <gps03155@naver.com>
2022-09-12 12:30:55 +0900

    Create 1259.java

---
+++ baekjoon/1259.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			String num = br.readLine();
+			
+			if(num.equals("0")) {
+				return;
+			}
+
+			StringBuilder builder = new StringBuilder(num);
+			String reverseNum = builder.reverse().toString();
+
+			System.out.println(num.equals(reverseNum)? "yes" : "no");
+		}
+	}
+}
