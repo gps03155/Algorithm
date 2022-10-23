1c88a0a1b4d57ac136fb79f735b9bc69bfe24c82
gps03155 <gps03155@naver.com>
2022-10-23 19:45:00 +0900

    Update 1316.java

--- baekjoon/1316.java
+++ baekjoon/1316.java
@@ -1,48 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.*;
-import java.util.StringTokenizer;
-import java.util.function.Predicate;
-import java.util.stream.*;
 import java.io.InputStreamReader;
-
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int testCase = Integer.parseInt(st.nextToken());
-		
-		List<Character> list = new ArrayList<>();
-		Boolean isGroup = false;
+		int test = Integer.parseInt(br.readLine());
 		int count = 0;
-
-		for(int i=0;i<testCase; i++) {
+		
+		for(int i=0; i<test; i++) {
 			String word = br.readLine();
+			boolean[] checkWord = new boolean[26];
+			boolean isGroup = true;
 			
-			list = new ArrayList<>();
-			int prev = 0;
+			checkWord[word.charAt(0) - 97] = true;
 			
-			for(int j=0;j<word.length();j++) {
-				
-				if(prev != word.charAt(j)) {
-					if(!list.contains(word.charAt(j))) {
-						list.add(word.charAt(j));
-						isGroup = true;
-					}
-					else {
-						isGroup = false;
-                       	 			break;
-					}
+			for(int j=1; j<word.length(); j++) {
+				if(checkWord[word.charAt(j) - 97] && word.charAt(j) != word.charAt(j -1)) {
+					isGroup = false;
+					break;
 				}
 				
-				prev = word.charAt(j);
+				checkWord[word.charAt(j) - 97] = true;
 			}
-
-			count = isGroup? count+1 : count;
+			
+			if(isGroup) count++;
 		}
 		
 		System.out.println(count);
-    }
+	}
 }
