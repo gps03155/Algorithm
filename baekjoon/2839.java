f52f87de56fc44ed5b55d7c3017fd60d1f40fc66
gps03155 <gps03155@naver.com>
2023-01-08 23:17:26 +0900

    Update 2839.java

--- baekjoon/2839.java
+++ baekjoon/2839.java
@@ -3,10 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static long gcd(long a, long b) {
-		return b > 0 ? gcd(b, a % b) : a;
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
