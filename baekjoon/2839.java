d9fce5e79df3dcc6e4cf61403f022d577f199b8b
gps03155 <gps03155@naver.com>
2023-01-08 23:16:13 +0900

    Create 2839.java

---
+++ baekjoon/2839.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static long gcd(long a, long b) {
+		return b > 0 ? gcd(b, a % b) : a;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int count = 0;
+
+		while (n >= 3) {
+			if (n % 5 == 0) {
+				count += (n / 5);
+				n %= 5;
+			}
+
+			if (n >= 3) {
+				n -= 3;
+				count++;
+			}
+
+			if (0 < n && n < 3) {
+				count = 0;
+			}
+		}
+
+		System.out.println(count == 0 ? -1 : count);
+	}
+}
