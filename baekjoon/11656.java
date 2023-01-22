0bd519325f9b45ab109047a4e2d98956e296e697
gps03155 <gps03155@naver.com>
2023-01-22 18:05:47 +0900

    Create 11656.java

---
+++ baekjoon/11656.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		String[] words = new String[s.length()];
+
+		for (int i = 0; i < words.length; i++) {
+			words[i] = s.substring(i, words.length);
+		}
+
+		Arrays.sort(words);
+
+		StringBuilder sb = new StringBuilder();
+		for (String word : words) {
+			sb.append(word).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+}
