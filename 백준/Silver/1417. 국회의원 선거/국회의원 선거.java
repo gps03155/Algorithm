654300e26e020e1a58293f115a3d9c308e7c9282
gps03155 <gps03155@naver.com>
2023-02-23 20:31:34 +0900

    [Silver V] Title: 국회의원 선거, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -1,47 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Integer[] nums = new Integer[n];
 
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(br.readLine());
-        }
-
-        if (nums.length == 1) {
-            System.out.println(0);
-            return;
-        }
-
-        int dasom = nums[0];
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        int dasom = Integer.parseInt(br.readLine());
         int count = 0;
 
-        Integer[] reverse = Arrays.copyOfRange(nums, 1, nums.length);
-        Arrays.sort(reverse, Collections.reverseOrder());
+        for (int i = 1; i < n; i++) {
+            queue.add(Integer.parseInt(br.readLine()));
+        }
 
-        while (true) {
-            boolean isDone = true;
-
-            for (int i = 0; i < reverse.length; i++) {
-                if (reverse[i] >= dasom) {
-                    count++;
-                    reverse[i]--;
-                    dasom++;
-                    isDone = false;
-                    Arrays.sort(reverse, Collections.reverseOrder());
-                    break;
-                }
-            }
-
-            if (isDone) {
-                break;
-            }
+        while (!queue.isEmpty() && dasom <= queue.peek()) {
+            dasom++;
+            count++;
+            queue.add(queue.poll() - 1);
         }
 
         System.out.println(count);
