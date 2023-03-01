b52145e0010664f508c6e2353f77b2d514192b10
gps03155 <gps03155@naver.com>
2023-03-01 22:19:28 +0900

    [Silver IV] Title: 알바생 강호, Time: 272 ms, Memory: 24080 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.max(arr[i] - i, 0);
+        }
+
+        System.out.println(sum);
+    }
+}
