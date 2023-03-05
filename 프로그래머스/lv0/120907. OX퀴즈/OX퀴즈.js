0a8b806d3f3cee54bafa222a8c5dcc1a12f768d4
gps03155 <gps03155@naver.com>
2023-03-05 22:44:49 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,8 +1,8 @@
 function solution(quiz) {
     const arr = [];
     for (let val of quiz) {
-      const val2 = val.split("=");
-      arr.push(eval(val2[0]) === Number(val2[1]) ? "O" : "X");
+      const [formula, result] = val.split("=");
+      arr.push(eval(formula) === Number(result) ? "O" : "X");
     }
 
     return arr;
