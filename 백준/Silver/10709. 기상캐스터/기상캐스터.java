b53067ca7c933e76ec43faa7fab5045d18fe84cc
gps03155 <gps03155@naver.com>
2023-03-08 21:30:14 +0900

    [Silver V] Title: 기상캐스터, Time: 476 ms, Memory: 20092 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+        int[][] sky = new int[h][w];
+
+        for (int i = 0; i < h; i++) {
+            int cloudIndex = -1;
+            String cloud = sc.next();
+
+            for (int j = 0; j < w; j++) {
+                char cha = cloud.charAt(j);
+
+                if (cha == 'c') {
+                    cloudIndex = j;
+                }
+
+                if (cloudIndex > -1) {
+                    sky[i][j] = j - cloudIndex;
+                } else if (cha == '.') {
+                    sky[i][j] = -1;
+                }
+            }
+        }
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                System.out.print(sky[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
