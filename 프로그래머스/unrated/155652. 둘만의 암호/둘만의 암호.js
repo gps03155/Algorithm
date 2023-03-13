7aee1ce15d138b97e50e94312f2d2a8cf6193f76
gps03155 <gps03155@naver.com>
2023-03-13 14:14:03 +0900

    [unrated] Title: 둘만의 암호, Time: 0.42 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,23 @@
+function solution(s, skip, index) {
+    let result = "";
+    for (let item of s.split("")) {
+      const arr = [];
+      let charIndex = 1;
+      while (arr.length < index) {
+        const ascii =
+          item.charCodeAt(0) + charIndex > 122
+            ? 97 + (item.charCodeAt(0) + charIndex - 123)
+            : item.charCodeAt(0) + charIndex;
+
+        const alpha = String.fromCharCode(97 + ((ascii - 97) % 26));
+
+        if (!skip.includes(alpha)) {
+          arr.push(alpha);
+        }
+        charIndex++;
+      }
+      result += arr[arr.length - 1];
+    }
+
+    return result;
+}
