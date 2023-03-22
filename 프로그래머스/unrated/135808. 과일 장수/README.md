6dd3bbd6e451f664707a06032f7b151452a30a14
gps03155 <gps03155@naver.com>
2023-03-23 08:27:27 +0900

    [unrated] Title: 과일 장수, Time: 0.35 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/README.md
+++ 프로그래머스/unrated/135808. 과일 장수/README.md
@@ -1,10 +1,10 @@
 # [unrated] 과일 장수 - 135808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808) 
 
 ### 성능 요약
 
-메모리: 73.4 MB, 시간: 0.45 ms
+메모리: 78.3 MB, 시간: 0.35 ms
 
 ### 구분
 
--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -5,21 +5,13 @@ import java.util.List;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        Arrays.sort(score);
+         Arrays.sort(score);
 
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
-        for (int i = score.length - 1; i >= 0; i--) {
-            if (list.size() < m) {
-                list.add(score[i]);
-            }
-            if (list.size() >= m) {
-                int p = Math.min(Collections.min(list), k);
-                answer += p * m;
-                list.clear();
-            }
+        for (int i = score.length - m; i >= 0; i -= m) {
+            answer += score[i] * m;
         }
-
+        
         return answer;
     }
 }
