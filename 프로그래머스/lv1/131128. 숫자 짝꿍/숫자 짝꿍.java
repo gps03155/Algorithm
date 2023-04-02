2021a9bd689acc46c3093150bd1cece5d77b46e4
gps03155 <gps03155@naver.com>
2023-04-02 11:03:34 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.18 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,35 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+
+
+class Solution {
+    public String solution(String x, String y) {
+        int[] xArr = new int[10];
+        int[] yArr = new int[10];
+
+
+        for (String s : x.split("")) {
+            xArr[Integer.parseInt(s)]++;
+        }
+
+        for (String s : y.split("")) {
+            yArr[Integer.parseInt(s)]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 9; i >= 0; i--) {
+            while (xArr[i] > 0 && yArr[i] > 0) {
+                sb.append(i);
+                xArr[i]--;
+                yArr[i]--;
+            }
+        }
+
+        if (sb.length() == 0) return "-1";
+        if (sb.charAt(0) == '0') return "0";
+
+        return sb.toString();
+    }
+}
