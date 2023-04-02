67e06acef6067bb20ee640e52081742f7a09d5f2
gps03155 <gps03155@naver.com>
2023-04-02 11:38:39 +0900

    [unrated] Title: 덧칠하기, Time: 0.75 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/README.md
+++ 프로그래머스/unrated/161989. 덧칠하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 덧칠하기 - 161989 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989) 
 
 ### 성능 요약
 
-메모리: 81 MB, 시간: 2.16 ms
+메모리: 84.8 MB, 시간: 0.75 ms
 
 ### 구분
 
--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,25 +1,11 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        boolean[] walls = new boolean[n];
-
-        for (int i = 0; i < walls.length; i++) {
-            walls[i] = true;
-        }
-
-        for (int i = 0; i < section.length; i++) {
-            walls[section[i] - 1] = false;
-        }
-
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (!walls[i]) {
+        int max = 0;
+        for (int i = 0; i < section.length; i++) {
+            if (max <= section[i]) {
+                max = section[i] + m;
                 count++;
-                int start = i;
-                int end = Math.min(i + m, walls.length);
-
-                for (int j = start; j < end; j++) {
-                    walls[j] = true;
-                }
             }
         }
 
