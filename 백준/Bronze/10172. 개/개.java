5dddaa228bfd9a62a01607ed0d2f005a6faaa77c
gps03155 <gps03155@naver.com>
2023-04-23 14:09:21 +0900

    [Bronze V] Title: 개, Time: 112 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ParseException {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
