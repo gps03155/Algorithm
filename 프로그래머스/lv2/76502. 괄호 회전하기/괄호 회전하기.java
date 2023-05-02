40319e70e89dbcc7bbf80b8a82b440e21a2797dc
gps03155 <gps03155@naver.com>
2023-05-03 02:15:28 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.55 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,56 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        Deque<String> deque = new LinkedList<>();
+
+        for (String str : s.split("")) {
+            deque.add(str);
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int count1 = 0, count2 = 0, count3 = 0;
+            String str = String.join("", deque);
+            Stack<String> stack = new Stack<>();
+
+            for (String cha : str.split("")) {
+                try {
+                    switch (cha) {
+                        case "(":
+                        case "{":
+                        case "[":
+                            stack.add(cha);
+                            break;
+                        case ")":
+                            if ("(".equals(stack.peek())) {
+                                stack.pop();
+                            }
+                            break;
+                        case "}":
+                            if ("{".equals(stack.peek())) {
+                                stack.pop();
+                            }
+                            break;
+                        case "]":
+                            if ("[".equals(stack.peek())) {
+                                stack.pop();
+                            }
+                            break;
+                    }
+                } catch (Exception e) {
+                    stack.add(cha);
+                    break;
+                }
+            }
+
+            deque.addLast(deque.pollFirst());
+
+            if (stack.isEmpty()) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
