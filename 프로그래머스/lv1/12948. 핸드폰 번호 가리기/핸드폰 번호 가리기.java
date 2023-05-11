d176413b2ff6267a769d960a5f7904189ec253d7
gps03155 <gps03155@naver.com>
2023-05-12 06:52:13 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.23 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,6 +1,8 @@
 class Solution {
     public String solution(String phone_number) {
-        int length = phone_number.length() - 4;
-        return phone_number.replaceAll("^\\d{" + length + "}", "*".repeat(length));
+        // int length = phone_number.length() - 4;
+        // return phone_number.replaceAll("^\\d{" + length + "}", "*".repeat(length));
+        
+        return phone_number.replaceAll("\\d(?=\\d{4})", "*");
     }
 }
