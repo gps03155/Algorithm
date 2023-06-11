eba80e5be6f54f264505320fc32ce075de677fac
gps03155 <gps03155@naver.com>
2023-06-12 07:14:00 +0900

    [Bronze II] Title: 방 배정하기, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.java
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -1,8 +1,7 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
@@ -10,9 +9,9 @@ public class Main {
         int c = sc.nextInt();
         int students = sc.nextInt();
 
-        for (int i = 1; i <= a; i++) {
-            for (int j = 1; j <= b; j++) {
-                for (int k = 1; k <= c; k++) {
+        for (int i = 1; i <= 50; i++) {
+            for (int j = 1; j <= 50; j++) {
+                for (int k = 1; k <= 50; k++) {
                     if (a * i + b * j + c * k == students) {
                         System.out.println(1);
                         return;
