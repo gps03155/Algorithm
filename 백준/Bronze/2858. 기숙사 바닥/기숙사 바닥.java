cd8fdf60faa4ab120494c62d225077655736c6ca
gps03155 <gps03155@naver.com>
2023-06-25 12:16:59 +0900

    [Bronze II] Title: 기숙사 바닥, Time: 268 ms, Memory: 18444 KB -BaekjoonHub

---
+++ 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int b = sc.nextInt();
+        int width = r + b;
+
+        int max = 0;
+        int min = 0;
+        for (int i = 3; i <= width; i++) {
+            for (int j = 3; j <= width; j++) {
+                if (i * j > width) break;
+                if (i * j == width) {
+                    if (r == (i * 2) + (j - 2) * 2) {
+                        max = Math.max(i, j);
+                        min = Math.min(i, j);
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(max + " " + min);
+    }
+}
