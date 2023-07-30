535d2d4695f8f901e1009b566bcd2481e8a628b8
gps03155 <gps03155@naver.com>
2023-07-30 21:08:05 +0900

    [Bronze III] Title: 저작권, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/2914. 저작권/저작권.java
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -5,19 +5,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
+        // case1
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        double x = a * b;
-        double answer = 0;
-        for (double i = x; i > 0; i--) {
-            double num = Math.ceil(i / a);
-            if (num < b) {
-                break;
-            }
-            answer = i;
-        }
+        System.out.println(a * (b - 1) + 1);
 
-        System.out.println((int) answer);
+        // case2
+//        double a = sc.nextDouble();
+//        double b = sc.nextDouble();
+//
+//        double x = a * b;
+//        double answer = 0;
+//        for (double i = x; i > 0; i--) {
+//            double num = Math.ceil(i / a);
+//            if (num < b) {
+//                break;
+//            }
+//            answer = i;
+//        }
+//
+//        System.out.println((int) answer);
     }
 }
