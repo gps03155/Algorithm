be49d6c7b0a4e073227e3c4fec62ca7eddba1b60
gps03155 <gps03155@naver.com>
2023-08-06 19:57:43 +0900

    [Bronze III] Title: 중간계 전쟁, Time: 292 ms, Memory: 19484 KB -BaekjoonHub

--- 백준/Bronze/4435. 중간계 전쟁/중간계 전쟁.java
+++ 백준/Bronze/4435. 중간계 전쟁/중간계 전쟁.java
@@ -1,34 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void print(int t, int gan, int sa) {
-        if (gan > sa) {
-            System.out.printf("Battle %d: Good triumphs over Evil", t);
-        } else if (sa > gan) {
-            System.out.printf("Battle %d: Evil eradicates all trace of Good", t);
-        } else {
-            System.out.printf("Battle %d: No victor on this battle field", t);
-        }
-        System.out.println();
-    }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        int[] gans = {1, 2, 3, 3, 4, 10};
-        int[] sas = {1, 2, 2, 2, 3, 5, 10};
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] h = {1,2,3,3,4,10};
+		int[] o = {1,2,2,2,3,5,10};
+		
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			System.out.print("Battle "+ (i+1) +": ");
+			int totaln = 0;
+			int totalo = 0;
+			
+			for(int j = 0; j < 6; j++)
+				totaln += sc.nextInt() * h[j];
+			for(int j = 0; j < 7; j++)
+				totalo += sc.nextInt() * o[j];
+			
+			if(totaln < totalo)
+				System.out.println("Evil eradicates all trace of Good");
+			else if(totaln > totalo)
+				System.out.println("Good triumphs over Evil");
+			else
+				System.out.println("No victor on this battle field");
+		}
 
-        for (int i = 0; i < t; i++) {
-            int gan = 0, sa = 0;
+	}
 
-            for (int j = 0; j < 6; j++) {
-                gan += gans[j] * sc.nextInt();
-            }
-
-            for (int k = 0; k < 7; k++) {
-                sa += sas[k] * sc.nextInt();
-            }
-            print(i + 1, gan, sa);
-        }
-    }
 }
