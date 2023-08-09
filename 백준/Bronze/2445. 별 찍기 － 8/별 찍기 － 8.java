8b9c613d722ff7a536a09807b8915a7da9307f70
gps03155 <gps03155@naver.com>
2023-08-09 21:40:12 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 276 ms, Memory: 18792 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("*".repeat(i) + " ".repeat(2 * (n - i)) + "*".repeat(i));
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            System.out.println("*".repeat(i) + " ".repeat(2 * (n - i)) + "*".repeat(i));
+        }
+    }
+}
