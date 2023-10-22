81a6566d622177c054b54a309044bb7ab2f84c92
gps03155 <gps03155@naver.com>
2023-10-22 15:22:48 +0900

    [Bronze II] Title: 강의실 예약 시스템, Time: 712 ms, Memory: 100020 KB -BaekjoonHub

--- 백준/Bronze/30019. 강의실 예약 시스템/강의실 예약 시스템.java
+++ 백준/Bronze/30019. 강의실 예약 시스템/강의실 예약 시스템.java
@@ -7,13 +7,10 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        Scanner sc = new Scanner(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-//        int n = sc.nextInt();
-//        int m = sc.nextInt();
 
         Map<Integer, Integer> room = new HashMap<>();
         StringBuilder sb = new StringBuilder();
@@ -22,14 +19,10 @@ public class Main {
             int k = Integer.parseInt(st.nextToken());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-//            int k = sc.nextInt();
-//            int s = sc.nextInt();
-//            int e = sc.nextInt();
 
             int prevEnd = room.getOrDefault(k, 0);
             boolean isAvailable = prevEnd <= s;
             sb.append(isAvailable ? "YES" : "NO").append("\n");
-//            System.out.println(isAvailable ? "YES" : "NO");
             if (isAvailable) {
                 room.put(k, e);
             }
