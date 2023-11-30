eef1c1f1425127a693285bb0cbec2bba5f2e186b
gps03155 <gps03155@naver.com>
2023-11-30 17:17:29 +0900

    [Silver V] Title: 라디오, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/3135. 라디오/라디오.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(nums);
+
+        int[] minNums = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            minNums[i] = Math.abs(end - nums[i]);
+        }
+
+        int min = Math.abs(start - end);
+        boolean isJump = false;
+
+        int current = start;
+        for (int i = 0; i < minNums.length; i++) {
+            if (minNums[i] < min) {
+                isJump = true;
+                current = nums[i];
+                min = minNums[i];
+            }
+        }
+
+        int count = 0;
+        if (isJump) {
+            count++;
+        }
+        if (!isJump || current != start) {
+            count += Math.abs(current - end);
+        }
+        System.out.println(count);
+
+    }
+}
