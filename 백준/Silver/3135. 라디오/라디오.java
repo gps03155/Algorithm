908079c9f04ed6486568d31b276d144ab246d05d
gps03155 <gps03155@naver.com>
2023-11-30 17:25:09 +0900

    [Silver V] Title: 라디오, Time: 124 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/3135. 라디오/라디오.java
+++ 백준/Silver/3135. 라디오/라디오.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,38 +11,46 @@ public class Main {
         int end = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(br.readLine());
 
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(br.readLine());
-        }
+        // case1. 최초 풀이
+//        int[] nums = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            nums[i] = Integer.parseInt(br.readLine());
+//        }
+//
+//        Arrays.sort(nums);
+//
+//        int[] minNums = new int[n];
+//        for (int i = 0; i < nums.length; i++) {
+//            minNums[i] = Math.abs(end - nums[i]);
+//        }
+//
+//        int min = Math.abs(start - end);
+//        boolean isJump = false;
+//
+//        int current = start;
+//        for (int i = 0; i < minNums.length; i++) {
+//            if (minNums[i] < min) {
+//                isJump = true;
+//                current = nums[i];
+//                min = minNums[i];
+//            }
+//        }
+//
+//        int count = 0;
+//        if (isJump) {
+//            count++;
+//        }
+//        if (!isJump || current != start) {
+//            count += Math.abs(current - end);
+//        }
+//        System.out.println(count);
 
-        Arrays.sort(nums);
-
-        int[] minNums = new int[n];
-        for (int i = 0; i < nums.length; i++) {
-            minNums[i] = Math.abs(end - nums[i]);
-        }
-
+        // case2. 개선 풀이
         int min = Math.abs(start - end);
-        boolean isJump = false;
-
-        int current = start;
-        for (int i = 0; i < minNums.length; i++) {
-            if (minNums[i] < min) {
-                isJump = true;
-                current = nums[i];
-                min = minNums[i];
-            }
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, Math.abs(Integer.parseInt(br.readLine()) - end) + 1);
         }
 
-        int count = 0;
-        if (isJump) {
-            count++;
-        }
-        if (!isJump || current != start) {
-            count += Math.abs(current - end);
-        }
-        System.out.println(count);
-
+        System.out.println(min);
     }
 }
