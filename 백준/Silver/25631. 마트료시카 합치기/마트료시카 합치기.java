f3a183b6089d9adf4ad3ff678d7effc3327df6dc
gps03155 <gps03155@naver.com>
2023-12-24 19:18:37 +0900

    [Silver V] Title: 마트료시카 합치기, Time: 148 ms, Memory: 14712 KB -BaekjoonHub

---
+++ 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        System.out.println(list.get(0).getValue());
+    }
+}
