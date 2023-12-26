67140527b13f4221ae8819c7e5e1cc65d63f01fc
gps03155 <gps03155@naver.com>
2023-12-26 13:32:37 +0900

    [Silver V] Title: A+B, Time: 256 ms, Memory: 16940 KB, Score: 1 point -BaekjoonHub

---
+++ 백준/Silver/27674. A＋B/A＋B.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            br.readLine();
+            String[] nums = br.readLine().split("");
+            Arrays.sort(nums, Collections.reverseOrder());
+            int num = Integer.parseInt(String.join("", Arrays.copyOfRange(nums, 0, nums.length - 1)));
+            System.out.println(num + Integer.parseInt(nums[nums.length - 1]));
+        }
+    }
+}
