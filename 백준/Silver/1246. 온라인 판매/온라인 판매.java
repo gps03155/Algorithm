7e5c6cd086c77206edcb1c8343f9ea9192ce6b9b
gps03155 <gps03155@naver.com>
2023-12-28 12:26:28 +0900

    [Silver IV] Title: 온라인 판매, Time: 152 ms, Memory: 16184 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] eggs = new int[n];
+        for (int i = 0; i < n; i++) {
+            eggs[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(eggs);
+
+        int egg = 0, maxPrice = 0;
+        for (int i = 0; i < n; i++) {
+            int count = Math.min(n - i, m);
+            int price = eggs[i] * count;
+            if (maxPrice < price) {
+                maxPrice = price;
+                egg = eggs[i];
+            }
+        }
+
+        System.out.println(egg + " " + maxPrice);
+    }
+}
