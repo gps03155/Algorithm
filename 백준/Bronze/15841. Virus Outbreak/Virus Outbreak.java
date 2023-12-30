497a3af24a8004a131a2e8c3040dd1c1e4e8da43
gps03155 <gps03155@naver.com>
2023-12-30 14:03:09 +0900

    [Bronze II] Title: Virus Outbreak, Time: 400 ms, Memory: 23808 KB -BaekjoonHub

--- 백준/Bronze/15841. Virus Outbreak/Virus Outbreak.java
+++ 백준/Bronze/15841. Virus Outbreak/Virus Outbreak.java
@@ -9,19 +9,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BigInteger[] cows = new BigInteger[491];
+        
+        cows[1] = BigInteger.ONE;
+        cows[2] = BigInteger.ONE;
+
+        for (int i = 3; i <= 490; i++) {
+            cows[i] = cows[i - 1].add(cows[i - 2]);
+        }
+
         while (true) {
             int num = Integer.parseInt(br.readLine());
             if (num < 0) {
                 break;
             }
-
-            cows[1] = BigInteger.ONE;
-            cows[2] = BigInteger.ONE;
-
-            for (int i = 3; i <= num; i++) {
-                cows[i] = cows[i - 1].add(cows[i - 2]);
-            }
-
             System.out.printf("Hour %d: %d cow(s) affected%n", num, cows[num]);
         }
     }
