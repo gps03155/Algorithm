81a922db3456a3b68ab2fa8acb7c78bc816cf63e
gps03155 <gps03155@naver.com>
2024-01-06 19:21:52 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14388 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
 
@@ -8,10 +9,10 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String s = br.readLine();
-        long ans = 0;
+        BigInteger ans = BigInteger.ZERO;
         for (int i = 0; i < s.length(); i++) {
-            ans += (s.charAt(i) - 96) * Math.pow(31, i);
+            ans = ans.add(BigInteger.valueOf(s.charAt(i) - 96).multiply(BigInteger.valueOf(31).pow(i)));
         }
-        System.out.println(ans);
+        System.out.println(ans.remainder(BigInteger.valueOf(1234567891)));
     }
 }
