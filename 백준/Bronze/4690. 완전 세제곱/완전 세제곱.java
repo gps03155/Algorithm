d21b4816e96dd8594390621ac71aba2c805d1c23
gps03155 <gps03155@naver.com>
2024-01-10 08:28:23 +0900

    [Bronze III] Title: 완전 세제곱, Time: 188 ms, Memory: 15148 KB -BaekjoonHub

---
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for (int a = 2; a <= 100; a++) {
+            for (int b = 2; b <= a; b++) {
+                for (int c = b; c <= a; c++) {
+                    for (int d = c; d <= a; d++) {
+                        if (a * a * a == b * b * b + c * c * c + d * d * d) {
+                            sb.append(String.format("Cube = %d, Triple = (%d,%d,%d)", a, b, c, d))
+                              .append("\n");
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
