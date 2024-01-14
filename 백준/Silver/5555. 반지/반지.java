de51a87a690b4ef53337e2e0b59c89b74e05c90d
gps03155 <gps03155@naver.com>
2024-01-14 17:48:21 +0900

    [Silver V] Title: 반지, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/5555. 반지/반지.java
+++ 백준/Silver/5555. 반지/반지.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
@@ -14,14 +11,9 @@ public class Main {
         int count = 0;
         for (int i = 0; i < n; i++) {
             String ring = br.readLine();
-            List<String> list = new ArrayList<>(Arrays.asList(ring.split("")));
-            for (int j = 0; j < ring.length(); j++) {
-                if (String.join("", list).contains(word)) {
-                    count++;
-                    break;
-                }
-                list.add(list.get(0));
-                list.remove(0);
+            ring += ring;
+            if (ring.contains(word)) {
+                count++;
             }
         }
         System.out.println(count);
