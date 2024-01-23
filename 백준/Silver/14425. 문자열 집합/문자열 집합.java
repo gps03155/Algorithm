b7c6ad20a8471707064281229adf4ae220a9f87b
gps03155 <gps03155@naver.com>
2024-01-23 22:38:28 +0900

    [Silver IV] Title: 문자열 집합, Time: 416 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<String> list = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            list.add(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            if (list.contains(br.readLine())) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
