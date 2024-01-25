fef033a6370e7da883238b62a484b09ce5ca2476
gps03155 <gps03155@naver.com>
2024-01-25 21:57:10 +0900

    [Silver III] Title: 합 구하기, Time: 536 ms, Memory: 51940 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.java
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -8,17 +8,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n + 1];
+        long[] pSum = new long[n + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        long[] pSum = new long[n + 1];
-        pSum[1] = arr[1];
-        for (int i = 2; i <= n; i++) {
-            pSum[i] = pSum[i - 1] + arr[i];
+            pSum[i] = pSum[i - 1] + Integer.parseInt(st.nextToken());
         }
 
         StringBuilder sb = new StringBuilder();
@@ -28,8 +22,8 @@ public class Main {
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            long sum = pSum[end] - pSum[start - 1];
-            sb.append(sum).append("\n");
+            sb.append(pSum[end] - pSum[start - 1])
+              .append("\n");
         }
         System.out.println(sb);
     }
