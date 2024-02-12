e3d52bc9727ba764ed80b4875cdf1e9cd3183a9c
gps03155 <gps03155@naver.com>
2024-02-12 16:47:26 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 1040 ms, Memory: 91436 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static List<Integer>[] list;
+    static int[] visited;
+
+    public static void dfs(int x, int depth) {
+        visited[x] = depth;
+        for (Integer i : list[x]) {
+            if (visited[i] == -1) {
+                dfs(i, depth + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList[n + 1];
+        visited = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            list[i] = new ArrayList<>();
+            visited[i] = -1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list[x].add(y);
+            list[y].add(x);
+        }
+
+        for (int i = 0; i < n + 1; i++) {
+            list[i].sort(Collections.reverseOrder());
+        }
+        dfs(r, 0);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < visited.length; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
