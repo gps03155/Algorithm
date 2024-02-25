49a217103a85f41a22e3f11bb9980eaba0421616
gps03155 <gps03155@naver.com>
2024-02-25 16:47:49 +0900

    [Bronze II] Title: 줄 세우기, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int people = 1;
+        while (st.hasMoreTokens()) {
+            int a = Integer.parseInt(st.nextToken());
+            list.add(a, people);
+            people++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            sb.append(list.get(i)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
