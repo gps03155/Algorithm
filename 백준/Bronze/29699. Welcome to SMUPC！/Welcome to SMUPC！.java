425fa5d5ceb0d5eaf14e9c733ee3f9c170aaa393
gps03155 <gps03155@naver.com>
2024-07-03 06:59:23 +0900

    [Bronze V] Title: Welcome to SMUPC!, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/29699. Welcome to SMUPC！/Welcome to SMUPC！.java
+++ 백준/Bronze/29699. Welcome to SMUPC！/Welcome to SMUPC！.java
@@ -7,13 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(br.readLine());
-
-        StringBuilder totalWord = new StringBuilder(word);
-        while (totalWord.length() <= size) {
-            totalWord.append(word);
-        }
-
-        System.out.println(totalWord.charAt(size - 1));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(word.charAt((n - 1) % 14));
     }
 }
