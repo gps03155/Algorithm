743222dd16a9ff662e17a1076cac8fec0465eb65
gps03155 <gps03155@naver.com>
2024-07-19 06:26:36 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 132 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String name = br.readLine();
+            if (name.contains("S")) {
+                System.out.println(name);
+            }
+        }
+    }
+}
